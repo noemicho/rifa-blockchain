5f4e5bc69c25d0c959e34212d601db788d2b2215
ennyagoc <ennyagomesoc@gmail.com>
2025-07-15 23:59:45 -0300

    Corrigindo codigo

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,13 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: process.env.INFURA_SEPOLIA_URL || "", 
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [], 
+      url: "https://sepolia.infura.io/v3/SUA_INFURA_KEY", 
+      accounts: [], 
     },
   },
 };
